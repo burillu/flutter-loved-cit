2bee3838bf3b20d8506d59bf3317bd8ef5f312cc
burillu <burillu90@hotmail.it>
2024-09-09 16:09:30 +0200

    section cit layout ok

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -23,19 +21,50 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget sectionCit() => SliverToBoxAdapter(
-          child: Stack(children: [
-        Container(
-          height: 650,
-          padding: EdgeInsets.all(20),
-          color: Colors.red,
-          child: Column(
-            children: [
-              Text(
+          child: Container(
+        padding: EdgeInsets.all(20),
+        child: Stack(children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.8,
+            // padding: EdgeInsets.all(20),
+            // color: Colors.red,
+            child: Center(
+              child: Text(
                 "Citation mega ultra filosofica strappalacrime",
-                style: TextStyle(fontSize: 28, fontStyle: FontStyle.italic),
+                style: TextStyle(
+                    fontSize: 35,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold),
               ),
-            ],
+            ),
           ),
-        ),
-      ]));
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.replay,
+                    color: Colors.grey[300],
+                    size: 50,
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.favorite_outline,
+                    color: Colors.red[300],
+                    size: 50,
+                  ),
+                )
+              ],
+            ),
+          )
+        ]),
+      ));
 }
